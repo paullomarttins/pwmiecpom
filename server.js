5cfb939ae025ec11e663e5b0344e3407c17fe23c
Paulo Martins <p.o.marttins@gmail.com>
2021-11-08 21:35:39 -0300

    Pratica de aula NodeJS PWM

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require ('http')
+
+const server = http.createServer (function (req, res){
+  res.end('Olá Mundo!')
+})
+
+const port = (process.env.PORT | 3000)
+server.listen(port, function(){
+  console.log(`Sevidor rodando em http://localhost:${port}`)
+})
